da9186b3ded378daaada281b22079b12852db8d2
qucui <qucui@cisco.com>
2018-04-16 15:55:01 +0800

    [SSDK-1879] Update Travis unit-test os and simulator settings for iOS10 and xcode 9

--- Source/Message/ListMessageOperation.swift
+++ Source/Message/ListMessageOperation.swift
@@ -194,10 +194,6 @@ class ListMessageOperation: Operation {
         self.returnSuccessResult()
     }
     
-    private func getBeforeString(){
-        
-    }
-    
     private func getBeforeTimeString(date: Date?)->String{
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
